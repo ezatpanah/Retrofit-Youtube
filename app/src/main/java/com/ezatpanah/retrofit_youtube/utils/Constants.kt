aafb7392e5b2669bc8c22a3ef2c5f69f22f52e61
Reyhaneh Ezatpanah <ezatpanah@gmail.com>
2022-09-01 19:49:30 +0300

    Update Constants.kt

--- app/src/main/java/com/ezatpanah/retrofit_youtube/utils/Constants.kt
+++ app/src/main/java/com/ezatpanah/retrofit_youtube/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.ezatpanah.retrofit_youtube.utils
 
 object Constants {
 
-    const val API_KEY = "e6626063d12904fa332eb63ee1fa1780"
+    const val API_KEY = "put your api key"
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
 
